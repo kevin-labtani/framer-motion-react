3157083b74a25256718a371013bddaeb854899da
Kevin Labtani <kevinlabtani@gmail.com>
2020-06-12 15:56:58 +0200

    modal animation

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const containerVariants = {
@@ -32,11 +32,17 @@ const childVariants = {
   },
 };
 
-const Order = ({ pizza }) => {
+const Order = ({ pizza, setShowModal }) => {
   const [showTitle, setShowTitle] = useState(true);
   setTimeout(() => {
     setShowTitle(false);
   }, 4000);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true)
+    }, 5000);
+  }, [setShowModal])
 
   return (
     <motion.div
